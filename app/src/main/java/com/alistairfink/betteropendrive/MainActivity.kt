e01e02caa1ca02ffd4b11ae85afa9af228a70f3c
Unknown <alistairfink@gmail.com>
2018-12-12 18:23:38 -0500

    Drawer?

---
+++ app/src/main/java/com/alistairfink/betteropendrive/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.alistairfink.betteropendrive
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
