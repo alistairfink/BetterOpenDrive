dcb7f8d7bbae1624f1a74aa73965288fe720ce44
Unknown <alistairfink@gmail.com>
2018-12-27 23:36:59 -0500

    updated file rename response model

--- app/src/main/java/com/alistairfink/betteropendrive/responseModels/FileRenameResponse.kt
+++ app/src/main/java/com/alistairfink/betteropendrive/responseModels/FileRenameResponse.kt
@@ -8,16 +8,16 @@ data class FileRenameResponse
         @SerializedName("Name") val Name: String,
         @SerializedName("GroupID") val GroupID: Int,
         @SerializedName("Extension") val Extension: String,
-        @SerializedName("Size") val Size: String,//Int,
-        @SerializedName("Views") val Views: String,//Int,
-        @SerializedName("Downloads") val Downloads: String,//Int,
-        @SerializedName("DateModified") val DateModified: String,//Long,
-        @SerializedName("Access") val Access: String,//Int,
+        @SerializedName("Size") val Size: Int,
+        @SerializedName("Views") val Views: Int,
+        @SerializedName("Downloads") val Downloads: Int,
+        @SerializedName("DateModified") val DateModified: Long,
+        @SerializedName("Access") val Access: Int,
         @SerializedName("Link") val Link: String,
         @SerializedName("DownloadLink") val DownloadLink: String,
         @SerializedName("StreamingLink") val StreamingLink: String,
         @SerializedName("TempStreamingLink") val TempStreamingLink: String,
         @SerializedName("Password") val Password: String,
         @SerializedName("Description") val Description: String,
-        @SerializedName("DirUpdateTime") val DirUpdateTime: String//Long
+        @SerializedName("DirUpdateTime") val DirUpdateTime: Long
 )
